a0969611b5c3012d769b9f81516cf579f22c52fb
renilson-medeiros <renilson.medeiros96@outlook.com>
2024-06-28 14:33:44 -0300

    CONTATO: ajuste na formatação dos dados

--- src/Contact.tsx
+++ src/Contact.tsx
@@ -2,7 +2,13 @@ import './App.css';
 
 const email = 'renilson.medeiros96@outlook.com';
 
-const EmailButton = ({ label, subject, body }) => {
+type EmailButtonProps = {
+  label: string;
+  subject: string;
+  body: string;
+};
+
+const EmailButton = ({ label, subject, body }: EmailButtonProps) => {
   const mailtoLink = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
 
   return (
@@ -11,7 +17,18 @@ const EmailButton = ({ label, subject, body }) => {
       href={mailtoLink}
     >
       <span>{label}</span>
-      <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-arrow-up-right">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="14"
+        height="14"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="lucide lucide-arrow-up-right"
+      >
         <path d="M7 7h10v10" />
         <path d="M7 17L17 7" />
       </svg>
